3e10a9ccbae9831f5ab01645b6f732c47bf52154
Benjamin Barahona <bbaraona@gmail.com>
2014-03-17 18:21:46 -0600

    first commit

---
+++ gen/io/kam/collage/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package io.kam.collage;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/io/kam/collage/CameraActivity.java
@@ -0,0 +1,16 @@
+package io.kam.collage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
